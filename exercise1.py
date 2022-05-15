f39bdf94a1a5eebd3f355f3000f1813306b9de3d
LEE-I-HUA <93990513+LEE-I-HUA@users.noreply.github.com>
2022-05-15 21:50:56 +0800

    Update exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -6,9 +6,25 @@ def square(n):
     else:
         return 0
         
-def e(n):
+def add(n):
     sum_=0
     for i in range(1,n+1):
         sum_=sum_+(1/square(n))
+        n=n-1
+    print(sum_)
+   
+
+# Euler's number evaluation
+
+def factorial(n):
+    if n>0:
+        return n*factorial(n-1)
+    else:
+        return 1
+
+def e(n):
+    sum_=1
+    for i in range(1,n+1):
+        sum_+=(1/factorial(n))
         n=n-1
     print(sum_)
